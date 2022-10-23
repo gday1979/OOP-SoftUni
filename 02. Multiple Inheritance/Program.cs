a560dafa1b62a2c82716e34317948b1066a049fd
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-10-23 20:24:18 +0300

    update

---
+++ 02. Multiple Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Multiple_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 03. Hierarchical Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Hierarchical_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
