5932b680e5cd191a4502f52bfcf1d4c3661fc43d
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-10-23 20:10:56 +0300

    Add project files.

---
+++ OOP SoftUni/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_SoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
