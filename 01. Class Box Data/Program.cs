a5ef6bf2637168d54ff7a5ff67557427f8e0aebc
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-10-24 20:49:47 +0300

    update

---
+++ 01. Class Box Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Class_Box_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 07. Military Elite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07._Military_Elite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
