be3af176de3728cc15c5a26e376e2d80d951ae68
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-10-30 13:37:11 +0200

    update

---
+++ 03. Players and Monsters/BladeKnight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class BladeKnight:DarkKnight
+    {
+        public BladeKnight(string Username,int Level):base(Username,Level)
+        {
+
+        }
+    }
+}
---
+++ 03. Players and Monsters/Hero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        public string Username { get; set; }
+        public int Level { get; set; }
+        public Hero(string userName, int level)
+        {
+            this.Username = userName;
+            this.Level = level;
+        }
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} UserName: {this.Username} Level: {this.Level}";
+        }
+    }
+}
