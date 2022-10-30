1f1ba84dc4f567295acba0e4575c38bd0cf7bea4
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-10-30 20:19:04 +0200

    update

---
+++ 01. Persons/Persons.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+    }
+}
---
+++ 01. Persons/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            int lines=int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                string[] cmdArg = Console.ReadLine().Split();
+                Person person = new Person(cmdArg[0], cmdArg[1], int.Parse(cmdArg[2]));
+                persons.Add(person);
+            }
+            persons.OrderBy(p => p.FirstName).ThenBy(p => p.Age).ToList().ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
