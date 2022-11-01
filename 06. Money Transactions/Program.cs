019ea6f63ec035ec0f127d9fc8e204082a813592
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-11-01 21:16:47 +0200

    update

---
+++ 06. Money Transactions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Money_Transactions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
