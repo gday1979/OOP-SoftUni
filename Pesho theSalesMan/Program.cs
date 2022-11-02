40091aa82eef4b95fd2abae60cd5871d3333bb49
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-11-02 21:40:39 +0200

    update

--- Pesho theSalesMan/Program.cs
+++ Pesho theSalesMan/Program.cs
@@ -6,7 +6,18 @@ namespace Pesho_theSalesMan
     {
         static void Main(string[] args)
         {
-            double numbers=double.Parse(Console.ReadLine());    
+            double monday=double.Parse(Console.ReadLine());
+            double tuesday=double.Parse(Console.ReadLine());
+            double wednesday=double.Parse(Console.ReadLine());
+            double thursday=double.Parse(Console.ReadLine());
+            double friday=double.Parse(Console.ReadLine());
+            double saturday=double.Parse(Console.ReadLine());
+            double sunday=double.Parse(Console.ReadLine());
+            double sum = monday + tuesday + wednesday + thursday + friday + saturday + sunday;
+            double average = sum / 7;
+            Console.WriteLine($"Total: {sum}");
+            Console.WriteLine($"Average: {average:f3}");
+
         } 
     }
 }
