0f04bf4732d072d5a6e3f99ba33cb0b5d0e4aa8f
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-11-03 20:09:11 +0200

    update

--- 05.FootballTeamGenerator/Program.cs
+++ 05.FootballTeamGenerator/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 
-namespace FootballTeamGenerator
+namespace _05.FootballTeamGenerator
 {
     public class Program
     {
