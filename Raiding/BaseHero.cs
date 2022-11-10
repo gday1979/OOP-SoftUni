25fe84882e1b06ddc3f275c8f56ed75fffa7f845
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-11-10 18:59:46 +0200

    update

---
+++ Raiding/BaseHero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public abstract class BaseHero
+    {
+        public string Name { get; set; }
+        public int Power { get; set; }
+        public BaseHero(string name,int power)
+        {
+            this.Name = name;
+            this.Power = power; 
+        }
+        public virtual string CastAbility()
+        {
+            return "";
+        }
+    }
+}
---
+++ Raiding/Druid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Druid : BaseHero
+    {
+        public Druid(string name) : base(name, 80)
+        {
+
+        }
+        public override string CastAbility()
+        {
+            return $"Druid - {this.Name} healed for {this.Power}";
+        }
+        
+        
+    }
+}
