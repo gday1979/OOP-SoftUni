d9eafa3b2d0a369525f14109480d3c4831d60ff9
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-11-22 20:27:25 +0200

    update

---
+++ CommandPatern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandPatern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
